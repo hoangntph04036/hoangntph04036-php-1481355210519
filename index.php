aa03165cf6d9c7d1d3c069d9efc9d819f77b8c84
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2016-12-10 14:36:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,112 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2016 mới ra mắt, Mu Online 2016, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2016,Mu Online 2016, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2016, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online</title>
+<meta name="description" content="MU SEASON2 - Sự Trở Lại Hoàn Hảo, Mu Mới Ra, Mu Moi Ra, Mu Hay Tháng 1 2 3 4 5 6 7 8 9 10 11 12,Mu Miễn Phí 100%, Gameplay Hấp Dẫn, GHRS Thấp, Mu Hay Nhat 2016"> 
+<meta name="keywords" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2015 mới ra mắt, Mu Online 2015, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2015,Mu Online 2015, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2015, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online"> 
+<meta name="abstract" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2015 mới ra mắt, Mu Online 2015, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2015,Mu Online 2015, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2015, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online" > 
+<meta name="Subject" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2015 mới ra mắt, Mu Online 2015, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2015,Mu Online 2015, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2015, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online"> 
+<link rel="canonical" href="/">
+<meta content="IE=Edge" http-equiv="X-UA-Compatible"> 
+<meta content="website" property="og:type" />
+<meta content="/" property="og:url" />
+<meta property="fb:app_id" content="134128110325497" />
+<meta property="og:image" content="/images/ads_facebook.png" />
+<meta itemprop="name" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2016 mới ra mắt, Mu Online 2016, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2016,Mu Online 2016, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2016, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online">
+<meta property="og:title" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2016 mới ra mắt, Mu Online 2016, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2016,Mu Online 2016, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2016, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online"/>
+<meta property="og:site_name" content="MU FPT 2016, MU mới ra liên tục cập nhật các phiên bản season mới nhất."/>
+<meta property="og:description" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2016 mới ra mắt, Mu Online 2016, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2016,Mu Online 2016, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2016, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online"/>
+<meta name="author" content="Mu Season 2 - Phiên bản mới, museason2, mu season2, mu season 2, muss2, muss 2, mu ss 2, Game MU Online 2016 mới ra mắt, Mu Online 2016, Game Mu Online ổn định và lâu dài, nơi tìm lại cảm giác chơi MU online Đích thực của các bạn ,Mu Moi,Mu moi ra, mu moi ra mat, mu moi ra lo, mu moi ra nhat hien nay, mu moi ra lo thang 3, mu moi ra 2016,Mu Online 2016, Mu Online , Mu Hay Nhat , Mu hay nhat hien nay, mu hay nhat viet nam, mu hay nhat the gioi, mu hay nhat 2016, Mu Online 2014 , Mu Dong Nguoi Choi , Mu Mien Phi , Mu Hot Nhat , Mu Vip , Mu Choi Lau Dai ,tai game mu,tai game mu online,download mu,download mu online, Mu Moi Ra, Mu Open Thang 1,2,3,4,5,6,7,8,9,10,11,12,2016, Mu Moi Ra, Mu Moi Open, Mu Sap Ra, Mu Alpha Test, Mu Hom Nay, Mu Online">
+<meta name="email" content="minhlatony@gmail.com">
+<meta name="Charset" content="UTF-8">
+<meta name="Expires" content="never">
+<meta name="Distribution" content="Global">
+<meta name="Designer" content="muss2.vn">
+<link rel="Shortcut Icon" href="muRedIcon.ico" type="image/x-icon">
+<link href="css/style.min.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+    <div class="wrap">
+        <div class="head">
+            <a class="logo" href="http://taigame.muss2.vn" target="_blank"><h1>MUSS2.VN</h1></a>
+            <ul class="menu cls">
+                <li class="border"><a href="http://muss2.vn/" target="_blank">Trang chủ</a></li>
+                <li style="background: url(images/icon.png) 0 2px no-repeat;padding-left: 25px;padding-left: 19px;height: 24px;"><a href="http://forum.muss2.vn/" target="_blank">Diễn đàn</a></li>
+            </ul>
+        </div>
+        <div class="main">
+			<a href="url">
+            <a class="link" href="http://forum.muss2.vn/showthread.php?14011-GHRS-Chinh-Thuc-TarKan-MUSS2VN" id="link1" target="_blank">Thông tin</a>
+            <a class="link" href="http://forum.muss2.vn/showthread.php?14007-Dua-Top-Alphatest-TarKan-MUSS2VN" id="link2" target="_blank">Sự kiện Alpha Test</a>
+            <a class="link" href="http://forum.muss2.vn/showthread.php?14009-Tong-Hop-Event-Dua-TOP-Chao-Mung-Cum-TARKAN-MUSS2VN" id="link3" target="_blank">Đua Top Open Beta</a>
+            <a class="link" href="http://forum.muss2.vn/showthread.php?14015-Chia-se-Facebook-nhan-GiftCode" id="link4" target="_blank">Sự kiện Open Beta</a>
+            <a class="link" href="http://taigame.muss2.vn" id="link5" target="_blank">Tải game</a>
+            <a class="link" href="http://id.muss2.vn" id="link6" target="_blank">Đăng ký</a>
+        </div>
+    </div>
+    
+			<div class="timer-area">
+				<div class="text"><li style="margin-left: 13px; text-align: center;">Thời gian còn lại</li></div>
+		<ul id="countdown">
+			<li>
+				<span class="days">00</span>
+			</li>
+			<li>
+				<span class="hours">00</span>
+			</li>
+			<li>
+				<span class="minutes">00</span>
+			</li>
+			<li>
+				<span class="seconds">00</span>
+			</li>
+		</ul>
+				<div class="text"><li style="margin-left: 82px;">Alpha Test lúc 14h00 ngày 13/12</li></div>
+				<div class="text"><li style="margin-left: 82px;">Open Beta lúc 14h00 ngày 17/12</li></div>
+
+	</div>
+    <script type="text/javascript" src="js/sq/lib/sq.core.js"></script>
+<!-- end timer-area -->		
+<script src="js/countdown.js"></script>
+<script type="text/javascript">
+$(window).ready(function(){
+    $(".ytb").click(function () {
+        $(".trailer").css('display','block');
+    });	
+    $(".close").click(function () {
+        $(".trailer").css('display','none');
+    });
+});
+</script>
+	<script>
+function setCookie(key, value) {  
+   var expires = new Date();  
+   expires.setTime(expires.getTime() + 60000);   
+   document.cookie = key + '=' + value + ';expires=' + expires.toUTCString()+';path=/'  
+   }  
+setCookie('visit', '1');  
+</script>
+<script type="text/javascript">
+		$(document).ready(function(){
+			$("#countdown").countdown({
+				date: "17 December 2016 14:00:00",
+				format: "on"
+			});
+		});
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47603543-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
+
