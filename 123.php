669e059b5fc7ae0866f38b6d9971b238ff1d6c41
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-05-03 13:19:59 +0700

    Update 123.php

--- 123.php
+++ 123.php
@@ -1,7 +1,6 @@
 <?php
-echo "hoang";
 $token = 'EAAAAAYsX7TsBAGOtoszzqLn7UnMFt4ORaEtcTzueA4iWQTjmaP2yatINCLBQJU5BLuuNLMv7KfEnz1Aax5CinTRUp8vhZA8a86oIgpeMKsb5QikZBiNP60ZCJyZBMJrqkQEY3m3m7htrXjeqTW02k7SisI2BpORrbFZC7X4iZCfEQPZCvmDAr2UeyBSYG7z23t2btiXCFkLce18wAvMfxiR'; //
-$limitnf=20; // 20 Status Ở NewFeed 1 Lần
+$limitnf=10; // 20 Status Ở NewFeed 1 Lần
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
