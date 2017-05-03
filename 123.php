72cdd81244847f0dac2dd4bfb3d4bdd55bb2e52d
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-05-03 13:24:19 +0700

    Update 123.php

--- 123.php
+++ 123.php
@@ -1,6 +1,6 @@
 <?php
 $token = 'EAAAAAYsX7TsBAGOtoszzqLn7UnMFt4ORaEtcTzueA4iWQTjmaP2yatINCLBQJU5BLuuNLMv7KfEnz1Aax5CinTRUp8vhZA8a86oIgpeMKsb5QikZBiNP60ZCJyZBMJrqkQEY3m3m7htrXjeqTW02k7SisI2BpORrbFZC7X4iZCfEQPZCvmDAr2UeyBSYG7z23t2btiXCFkLce18wAvMfxiR'; //
-$limitnf=10; // 20 Status Ở NewFeed 1 Lần
+$limitnf=10; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
@@ -8,6 +8,7 @@ $camxuc= array('LOVE');
 $mess=$camxuc[rand(0,count($camxuc)-1)];
 echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
 }
+
 
 function puaru($url){
 $data = curl_init();
@@ -18,3 +19,4 @@ curl_close($data);
 return $hasil;
 }
 ?>
+							
