defef1a77fff39db219011576d87758dafb4d9e1
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-05-10 23:01:31 +0700

    Update dong.php

--- dong.php
+++ dong.php
@@ -1,6 +1,6 @@
 <?php
 echo "Dong xi da 03/05/2017 TIM";
-$token = 'EAAAAAYsX7TsBAHfRa3ZBGTnWV49jVqMcq388FX62lrslLwKJqBGP10v60M8lLZBdoco3pjLsCPS9xZBxZCgRLwDExfivxUif6jda5gJBP6HIJqI47WZB0PC0SEgszf1tYjNwsrSNDQcZCgltziTUaf2HXLoeT5HmjiLebzylohIgoitZBWwWp06ZBAEloeDWvaG2oDEduXrDzkCrMrWxCD0C'; //
+$token = 'EAAAAAYsX7TsBAI1UQLkhyZAk2yztDPcQsIvsbJhw8bgbV2UiWHEI70Whlq620wsaZC5o7FxzouCOgfhFGD3Wh1ZASL9Y9ZCrGX3nsme68XF10aKJejhWdYaWmdYeOmPedADstM3htu3ZBWYq8ZAB3zRL9j5xi0crxIC1AOxRNEtEQ7cnIDL81TN7MLrpJ0lSA7ZCyimg60rjgZDZD'; //
 $limitnf=5; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
