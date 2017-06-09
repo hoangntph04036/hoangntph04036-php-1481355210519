c4e3eaae1c8301a4e4b5a79e92a4dc03cbee1a47
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-06-09 19:19:05 +0700

    Create index.php

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
-echo "thanh Hue";
-$token = 'EAAAAAYsX7TsBAE2rif5yXmi6IZBtPPrDP6RpZCacbQ94LMlPnKxfNYrJYzWrGa71nz5M9ZAZCKx4VkB21gQMZB89xXiPgHBoUXv4QWFNs7cGjZAaJadp1LoxLEIX0vYNoQH3T83PrE6jOeMIvWeQgJ5a6ZAiIDIcKcSZCXhJs0gMOG6qsG7wg0C8yHetwTBgikP4IKxroPoXjQDUT2ahWUNU'; //
-$limitnf=5; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
-$puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
-for($i=1;$i<=count($puaru[data]);$i++){
+$token = 'EAAAAAYsX7TsBAEgo0yeemMBTsBIFaZAUNusbT1cpU1wyGf9z9WCJ2XLrBENd0EZCo9Y27wKgB3DC8pEd7VHoF9uGjFQvZB1iuYrCc71iakU4kUEbw2JDbHOhcZAfyFvkjudbID0BhK7zEtAvfVAgLCNMx7oxlJCr8a37ZBBEM8lTVVHwWlnauKvZBJugu1e8jjkTSAb44rmn6FZALYU3a0Q'; //Token lấy ở phuongbach.com
+$limitnf=20; // 20 Status Ở NewFeed 1 Lần
+$cgcst=json_decode(cgcst('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
+for($i=1;$i<=count($cgcst[data]);$i++){
 set_time_limit(0);
-$camxuc= array('LIKE');
+$camxuc= array('LOVE'); //Các cảm xúc có thể thay đổi là LOVE,HAHA,ANGRY,WOW,SAD,LIKE,THANKFUL
 $mess=$camxuc[rand(0,count($camxuc)-1)];
-echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
-
+echo cgcst('https://graph.facebook.com/'.$cgcst[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
 }
 
-
-function puaru($url){
+function cgcst($url){
 $data = curl_init();
 curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($data, CURLOPT_URL, $url);
 $hasil = curl_exec($data);
 curl_close($data);
 return $hasil;
-
 }
-?>
-							
+?>							
