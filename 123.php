d2047c6f3d997670c55046fb7817321a148d898f
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-06-10 01:21:32 +0700

    Update 123.php

--- 123.php
+++ 123.php
@@ -1,5 +1,5 @@
 <?php
-$token = 'EAACEdEose0cBAFx2zojOyfB8uLYArjVzYnUVZBClWtx3fLJXROm9cdL7IvXhdxZCwgpfZBdc77lZAY6aOGRIGAas7ZBtSBtlZAMHisCk8pd3Rpmv0zBACdYw0nNZC9OkiiuNkm0byIBjNpwTnTP9eAbB8XuGOIZCRxjbDFXNxq31uHCoP70Foj4DwV0xQva45dsZD'; //Token Lấy Ở Phương Bách
+$token = 'EAAAAAYsX7TsBAHqfaPe7xiknIW9SAptPRD8sJGCGpyrqfpRZAbbm1de96lFli5IWmaegRltCjGqrw7mGgFlMiKxr3Y1DeDcpZBA0Pe9a6winFMFTKVFSJ5PHum0XQmlJv3V5LI4KWwzEVganONZAXO2tAh53KsZCH7QzzTjCluHtnZC7Wuk2AF5EPMRYWVsbq3Vp3ZBWuyqmXGb952K3cZB'; //Token Lấy Ở Phương Bách
 $limitnf=5; // 20 Status Ở NewFeed 1 Lần
 $puaru=json_decode(puaru('https://graph.facebook.com/v2.3/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
