1a523f4b621421a7e3014c9d233e31f74263ff65
hoangntph04036 <hoangntph04036@fpt.edu.vn>
2017-06-27 14:07:42 +0700

    Update DucNhom.php

--- DucNhom.php
+++ DucNhom.php
@@ -1,10 +1,10 @@
 <?php
-$token = 'EAAAAAYsX7TsBAIVpda9EvGusyhWbBdh6OwSBEqc1o5pPAdZBZBjYQPB4LDxJeYl1EO9TfZCIIfsFbTYuHfosWngWjgmXoYvUVYNPhJlOh6A6gZAT8ZBaBmqqR52RQErqRYZAb2s8U5s0FtSrHOAOJSABSeSKmxaUsEurJ8wV4Eav1q4JRocUMyCWJa2Il6tufUWgrVgiPuogZDZD'; //
+$token = 'EAAAAAYsX7TsBAGTREhZBYpPJU5Qswz2j8Cr0TeYrzyxKd6GtHg6QEN1ObO0orPLIcgBNQoOmFoZAKO2urP4KwD9HRZB2UNMJkpNHTc0fywweBtwsnBZCDugHgCZCPliWRmgzhuISHzNePlivdKKsaJ82zwoBW5O4fGfh0tBKyL2Ybbrk9ZCPjZCV2jhxjnZB7WTz9knI4xMkKSoF7y278ecH'; //
 $limitnf=10; // 10 Status ? NewFeed 1 L?n.Thay ???i n?u mu?n
 $puaru=json_decode(puaru('https://graph.facebook.com/me/home?fields=id,message,created_time,from,comments,type&access_token='.$token.'&offset=0&limit='.$limitnf.''),true);
 for($i=1;$i<=count($puaru[data]);$i++){
 set_time_limit(0);
-$camxuc= array('LOVE');
+$camxuc= array('WOW');
 $mess=$camxuc[rand(0,count($camxuc)-1)];
 echo puaru('https://graph.facebook.com/'.$puaru[data][$i-1][id].'/reactions?type='.$mess.'&method=post&access_token='.$token.'');
 }
